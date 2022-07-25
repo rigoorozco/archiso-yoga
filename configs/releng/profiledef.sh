bac495eabdf66bed77e0e42ab0079c941b47991e
Alex R <m.alexanderrobinson@yahoo.com>
2022-07-25 11:26:48 -0500

    revert releng config to original

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -3,17 +3,16 @@
 
 iso_name="archlinuxarm"
 iso_label="ARCH_AARCH64_$(date +%Y%m)"
-iso_publisher="Alex R"
-iso_application="Generic ARM64 Arch Linux Live/Rescue Disk"
+iso_publisher="Jack Myers"
+iso_application="Generic ARM64 Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('uefi-aarch64.systemd-boot.esp' 'uefi-aarch64.systemd-boot.eltorito')
 arch="aarch64"
 pacman_conf="pacman.conf"
-airootfs_image_type="erofs"
-#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
-airootfs_image_tool_options=('-zlz4hc,12')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'arm' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
